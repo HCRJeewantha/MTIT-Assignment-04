b90b24af3d855fa5e6c2bd03bb7fa8be1aa05d52
HCRJeewantha <52495212+HCRJeewantha@users.noreply.github.com>
2021-05-27 02:03:41 +0530

    Update PaymentResponse.java

--- payment-service/payment-service/src/main/java/com/sliit/mtit9/paymentservice/dto/PaymentResponse.java
+++ payment-service/payment-service/src/main/java/com/sliit/mtit9/paymentservice/dto/PaymentResponse.java
@@ -3,27 +3,7 @@ package com.sliit.mtit9.paymentservice.dto;
 public class PaymentResponse {
     private Integer Id;
     private Integer UserId;
-    private Integer ProductId;
-    private Integer Quantity;
     private Float Total;
-    private Integer CartId;
-    private String ProductName;
-
-    public String getProductName() {
-        return ProductName;
-    }
-
-    public void setProductName(String productName) {
-        ProductName = productName;
-    }
-
-    public Integer getCartId() {
-        return CartId;
-    }
-
-    public void setCartId(Integer cartId) {
-        CartId = cartId;
-    }
 
     public Integer getId() {
         return Id;
@@ -41,21 +21,6 @@ public class PaymentResponse {
         this.UserId = userId;
     }
 
-    public Integer getProductId() {
-        return ProductId;
-    }
-
-    public void setProductId(Integer productId) {
-        this.ProductId = productId;
-    }
-
-    public Integer getQuantity() {
-        return Quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        Quantity = quantity;
-    }
 
     public Float getTotal() {
         return Total;
